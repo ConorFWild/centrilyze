d4efe7b660533e9d3a2a3fa1ae555a8ccc059d30
cwild <conor.wild@sky.com>
2022-02-08 17:27:39 +0000

    updated

--- centrilyze/__init__.py
+++ centrilyze/__init__.py
@@ -2,7 +2,7 @@ from centrilyze.constants import *
 from centrilyze.dataset import ImageDataset
 from centrilyze.fs import CentrioleImageFiles
 from centrilyze.hmm import HMM
-from centrilyze.model import Model
+from centrilyze.model import CentrioleImageModel
 from centrilyze.stats import get_confusion_matrix
 from centrilyze.test import annotate
 from centrilyze.transforms import label_to_tensor, image_transform, target_transform
